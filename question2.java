da906973ac7a3f1e3d60ab3473e68f1705c7dc30
Shavedha Yuvarajan <93427376+Shavedha@users.noreply.github.com>
2023-03-14 19:33:23 +0530

    Add files via upload

---
+++ question2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class question2 {
+    public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+        int n=scan.nextInt();
+        for(int i=n;i>=1;i--)
+        {
+            for(int j=n;j>=i;j--)
+                System.out.print(" ");
+            for(int k=1;k<=2*i-1;k++)
+                System.out.print("*");
+
+            System.out.println();
+        }
+    }
+}
